3e5b543dc16cb40b85529573661dd2b3e6689e95
Antón Valladares <anton.valladares@udc.es>
2020-09-24 18:00:57 +0200

    init repo

---
+++ src/main/webapp/resources/js/initialize.js
@@ -0,0 +1,12 @@
+/**
+ * FD Spring MVC scrollad app javascript initialization.
+ * Copyright 2020 Information Retrieval Lab
+ * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * 
+ * Loads all the Javascript functions used by the site.
+ */
+
+/**
+ * Initializes scripts when the document loads.
+ */
+$(document).ready(function() {});
---
+++ src/test/java/es/udc/fi/dc/fd/test/integration/service/ITDefaultSeleniumService.java
@@ -0,0 +1,105 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2020 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package es.udc.fi.dc.fd.test.integration.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath:context/service.xml",
+        "classpath:context/persistence.xml", "classpath:context/application-context.xml" })
+@TestPropertySource({ "classpath:config/persistence-access.properties" })
+public final class ITDefaultSeleniumService {
+
+    private static final String BASE_URL = "http://localhost:7080/scrollad/";
+
+    private static WebDriver driver;
+
+    private ITDefaultSeleniumService() {
+        super();
+    }
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    public void setUpTest() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    public void tearDownTest() {
+        driver.close();
+    }
+
+    @Test
+    public void testMainPage() {
+        driver.get(BASE_URL);
+
+        WebElement mainSection = driver.findElement(By.id("main-section"));
+        assertNotNull(mainSection);
+    }
+
+    @Test
+    public void testListing() {
+        driver.get(BASE_URL + "entity/list");
+
+        WebElement mainSection = driver.findElement(By.id("main-section"));
+        assertNotNull(mainSection);
+    }
+
+}
